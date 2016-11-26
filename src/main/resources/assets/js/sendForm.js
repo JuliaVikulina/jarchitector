bc51fa4340d5fe85d9555b1a41388bcf95ccbdf6
Julia Vikulina <julia.vikulina@gmail.com>
2016-11-26 19:09:41 +0300

    json is received; parameters are sent

--- src/main/resources/assets/js/sendForm.js
+++ src/main/resources/assets/js/sendForm.js
@@ -1,24 +1,65 @@
 $('#submit').click(function(){
     console.log("sending parameters...");
-    var formElement = document.querySelector("form");
 
-    var formData = new FormData(formElement);
-    console.log(formData);
+    var width = document.getElementById("width").value;
+    var height = document.getElementById("height").value;
+    var seed = document.getElementById("seed").value;
+
+    var mess = "width="+ width
+     + "&" + "height=" + height
+     + "&" + "seed=" + seed;
+
+    console.log(mess);
 
     $.ajax({
         url: "http://192.168.1.46:8080/generate",
         type: 'POST',
-        data: formData,
+        data: mess,
+                // data: $('#formdata').serialize(),
+
         async: false,
         cache: false,
-        contentType: "application/x-www-form-urlencoded",
-        processData: false,
+        // contentType: "application/x-www-form-urlencoded",
+        // processData: false,
         success: function (response) {
-            console.log(response);
-            alert(response);
+            console.log(JSON.stringify(response));
+            // alert(response);
+            $("#response").val(JSON.stringify(response));
         },
         error: function(message) {
             alert("error\n" + message.statusText);
         }
       });
+
+    // var formElement = document.querySelector("form");
+
+    // var width = document.getElementById("width").value;
+    // var height = document.getElementById("height").value;
+
+    // // var formData = new FormData(formElement);
+
+    // // formData.append("width", width);
+    // // formData.append("height", height);
+
+    // var form = {"width": width, "height": height};
+
+
+    // console.log(form);
+
+    // $.ajax({
+    //     url: "http://192.168.1.46:8080/generate",
+    //     type: 'POST',
+    //     data: form,
+    //     async: false,
+    //     cache: false,
+    //     contentType: "application/x-www-form-urlencoded",
+    //     processData: false,
+    //     success: function (response) {
+    //         console.log(response);
+    //         alert(response);
+    //     },
+    //     error: function(message) {
+    //         alert("error\n" + message.statusText);
+    //     }
+    //   });
 });
