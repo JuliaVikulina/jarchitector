7e457d8333bb6971f2a2576ec07bbbbaa8a71332
daniil <bubnov.d.e@gmail.com>
2016-11-29 20:53:23 +0300

    amazing maze (part 1)

--- src/main/java/org/dandj/core/generation/StageGenerator.java
+++ src/main/java/org/dandj/core/generation/StageGenerator.java
@@ -2,22 +2,25 @@ package org.dandj.core.generation;
 
 import org.dandj.utils.AsciiPrinter;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 import static org.dandj.api.API.*;
 
 public class StageGenerator {
-    public static Stage createStage(Stage.Builder stageBuilder, Random r) {
-        int roomSizeX = 3; //todo make random
-        int roomSizeY = 3; //todo make random
-        int roomTries = 5; //todo make random
+    public static Stage createStage(Stage.Builder sb, Random r) {
+        int roomSizeX = 3; //todo make input parameter
+        int roomSizeY = 3; //todo make input parameter
+        int roomTries = 5; //todo make input parameter
 
-        int xrange = stageBuilder.getWidth();
-        int yrange = stageBuilder.getHeight();
+        int xrange = sb.getWidth();
+        int yrange = sb.getHeight();
 
-        int[][] stage = new int[xrange][];
+        // simple two dimensional array to represent the layout
+        int[][] stageGrid = new int[xrange][];
         for (int i = 0; i < xrange; i++) {
-            stage[i] = new int[yrange];
+            stageGrid[i] = new int[yrange];
         }
 
         createRoom:
@@ -28,7 +31,7 @@ public class StageGenerator {
             // check that new room does not overlap with existing ones
             for (int x = 0; x < roomSizeX; x++) {
                 for (int y = 0; y < roomSizeY; y++) {
-                    if (stage[roomX + x][roomY + y] != 0) {
+                    if (stageGrid[roomX + x][roomY + y] != 0) {
                         continue createRoom;
                     }
                 }
@@ -36,14 +39,32 @@ public class StageGenerator {
             Region.Builder regionBuilder = Region.newBuilder();
             for (int x = 0; x < roomSizeX; x++) {
                 for (int y = 0; y < roomSizeY; y++) {
-                    stage[roomX + x][roomY + y] = 1;
-                    regionBuilder.addCells(Cell.newBuilder()
-                            .setX(roomX + x).setY(roomY + y).build());
+                    stageGrid[roomX + x][roomY + y] = 1;
+                    regionBuilder.addCells(
+                            Cell.newBuilder().setX(roomX + x).setY(roomY + y).build());
                 }
             }
-            stageBuilder.addRegions(regionBuilder.build());
+            sb.addRegions(regionBuilder.build());
         }
-        System.out.println(AsciiPrinter.printStage(stageBuilder));
-        return stageBuilder.build();
+
+        if (!sb.getRegionsList().isEmpty()) {
+            // carve corridors to regions:
+            Set<Region> notConnected = new HashSet<>(sb.getRegionsList());
+            Set<Region> connected = new HashSet<>();
+
+            notConnected.remove(sb.getRegions(0));
+            connected.add(sb.getRegions(0));
+            while (!notConnected.isEmpty()) {
+                // we carve a maze starting from any connected region
+                // if a carved maze connects an yet unconnected region,
+                // add it and mentioned region to connected and remove from notConnected
+            }
+        }
+        System.out.println(AsciiPrinter.printStage(sb));
+        return sb.build();
+    }
+
+    private static Region carveMaze() {
+        return Region.newBuilder().build();
     }
 }
